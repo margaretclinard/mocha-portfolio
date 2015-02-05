f5dac7155b2bcf93c76167c41ea7fc13aaf66a29
Margaret Clinard <margaret.clinard@gmail.com>
2015-02-05 12:28:31 -0600

    added test for appending row to stock table

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,13 @@ function getStock(symbol, cb) {
 function addStockToTable(stock) {
   var $row = $('<tr></tr>');
 
+  $row.append('<td>' + stock.Name + '</td>');
+  $row.append('<td>' + stock.Symbol + '</td>');
+  $row.append('<td>' + stock.LastPrice + '</td>');
+
   $('tbody').append($row);
+
+  return $row;
 }
 
 function hello() {
